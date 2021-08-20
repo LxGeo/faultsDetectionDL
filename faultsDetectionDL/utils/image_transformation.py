59b3c5064630fc0465e4c954f888c0cddf3cfb64
Bilel Kanoun <bilel.kanoun@uniparthenope.it>
2021-08-20 21:18:17 +0200

    adding recurse image transformation

--- faultsDetectionDL/utils/image_transformation.py
+++ faultsDetectionDL/utils/image_transformation.py
@@ -56,4 +56,60 @@ class Trans_gaussian_noise(Image_Transformation):
         return (image1_t, gt1)
 
 
-images_transformations_list=[Trans_Rot90, Trans_Rot180, Trans_Rot270, Trans_Flipud, Trans_fliplr, Trans_gaussian_noise]
+images_transformations_list=[Trans_Rot180, Trans_Flipud, Trans_fliplr, Trans_gaussian_noise]
+TDO = { images_transformations_list[i].__name__ : i for i in range(len(images_transformations_list)) }
+to_ignore_trans_lists=[ [0,1,2] ]
+
+class recurse_transform():
+    
+    _images_transformations_list = images_transformations_list
+    
+    def __init__(self, image_couple, max_trans=20):
+        self.image_couple = image_couple
+        self.max_trans = max_trans
+        self.all_transformed = []
+        self.trans_indices=[]
+        
+    def run_recurse(self, couple_image_gt, c_Ts, names_idx):
+        """
+        """
+        if len(c_Ts)==1:
+            c_names_indices = names_idx+[TDO[c_Ts[0].__name__]]
+            if any([all([ c in c_ignore_list for c in c_names_indices ]) for c_ignore_list in to_ignore_trans_lists]):
+                return
+            self.trans_indices.append(c_names_indices)
+            self.all_transformed.append( c_Ts[0].apply_transformation(*couple_image_gt ))
+            return
+        
+        for c_trans_index in range(len(c_Ts)) :
+            
+            c_trans = c_Ts[c_trans_index]
+            c_names_indices = names_idx+[TDO[c_trans.__name__]]
+            if any([all([ c in c_names_indices for c in c_ignore_list ]) for c_ignore_list in to_ignore_trans_lists]):
+                continue
+            c_trans_couple = c_trans.apply_transformation(*couple_image_gt)
+            self.trans_indices.append(c_names_indices)
+            self.all_transformed.append( c_trans_couple )
+            self.run_recurse(c_trans_couple, c_Ts[c_trans_index+1:], c_names_indices)
+        return
+
+            
+if __name__ == "__main__":
+    
+    from skimage.io import imread
+    
+    im1_path = "/home/dell/Documents/LxGeo/repos/faultsDetectionDL/data/processed/partition_512_site_A_B/valid/image/Site_A_rot_45_204.tif"
+    gt1_path = "/home/dell/Documents/LxGeo/repos/faultsDetectionDL/data/processed/partition_512_site_A_B/valid/gt/Site_A_rot_45_204.tif"
+    
+    im1 = imread(im1_path)
+    gt1= imread(gt1_path)
+    
+    RT = recurse_transform((im1, gt1))
+    RT.run_recurse(RT.image_couple, images_transformations_list, [])
+    
+    all_t = RT.get_all_transformed()
+    
+    
+    
+    
+    
