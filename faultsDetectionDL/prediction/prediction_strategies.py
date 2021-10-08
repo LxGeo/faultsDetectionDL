bec8206fcf142bfdf7085dac6af401d61376172f
cherif.abedrazek@gmail.com <cherif.abedrazek@gmail.com>
2021-10-08 16:32:41 +0200

    tiled prediction creation && fxing blocks count

--- faultsDetectionDL/prediction/prediction_strategies.py
+++ faultsDetectionDL/prediction/prediction_strategies.py
@@ -30,6 +30,8 @@ from rasterio.windows import Window
 from shapely.geometry import box
 from rasterio.plot import reshape_as_raster, reshape_as_image
 from rasterio import mask
+from tqdm import tqdm
+
 
 class PatchOverlapStrategy():
     
@@ -43,10 +45,11 @@ class PatchOverlapStrategy():
         self.pad_count = (self.patch_size-self.pred_size)//2
         
         out_profile = rio_dataset.profile.copy()
-        out_profile.update(count=1, dtype= rio.uint8 if n_classes>1 else rio.float32)
+        out_profile.update(tiled=True, blockxsize=self.pred_size, blockysize=self.pred_size, count=1, dtype= rio.uint8 if n_classes>1 else rio.float32)
         
         with tempfile.NamedTemporaryFile(delete=False, suffix=".tif") as out_tempfile:
             self.out_rio_dst = rio.open(out_tempfile.name, mode='w+', **out_profile)
+            self.whole_dataset_window = Window(0,0, self.out_rio_dst.width, self.out_rio_dst.height)
     
     def __del__(self):
         self.out_rio_dst.close()
@@ -60,13 +63,13 @@ class PatchOverlapStrategy():
             reshape_as_image(self.rio_dataset.read())[:,:,:self.num_bands],
             ((self.pad_count, self.patch_size), (self.pad_count, self.patch_size), (0,0)) 
             )
-        
+                
         IMG_HEIGHT = self.rio_dataset.height
         IMG_WIDTH = self.rio_dataset.width
     
         # Define the grid blocks
-        Nx_blocks = math.floor(IMG_WIDTH/self.pred_size)
-        Ny_blocks = math.floor(IMG_HEIGHT/self.pred_size)
+        Nx_blocks = math.ceil(IMG_WIDTH/self.pred_size)
+        Ny_blocks = math.ceil(IMG_HEIGHT/self.pred_size)
     
         big_patches_array=[] ## 100% patch size
         small_windows=[] ## overlap_ratio % patch size
@@ -98,8 +101,16 @@ class PatchOverlapStrategy():
         big_patches_preds = self.model.predict(big_patches_array)
         small_patches_preds = big_patches_preds[:, self.pad_count:-self.pad_count,self.pad_count:-self.pad_count,: ]
         
-        for patch_index ,c_patch_window in enumerate(small_windows):
-            self.out_rio_dst.write(reshape_as_raster(small_patches_preds[patch_index].astype(self.out_rio_dst.meta["dtype"])), window = c_patch_window)
+        
+        for patch_index ,c_patch_window in enumerate(tqdm(small_windows, desc="Writing prediction")):
+            # out of range case check
+            intersection_window = c_patch_window.intersection(self.whole_dataset_window)
+            array_to_save = small_patches_preds[patch_index, :intersection_window.width, :intersection_window.height, :]
+            
+            self.out_rio_dst.write(reshape_as_raster(array_to_save.astype(self.out_rio_dst.meta["dtype"])), window = intersection_window)
+        
+        self.out_rio_dst.close()
+        self.out_rio_dst = rio.open(self.out_rio_dst.name, "r")
 
 
 
