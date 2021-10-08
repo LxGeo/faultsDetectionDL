7f993b61f0a358c3854979687b7a992c7c4b1aaf
cherif.abedrazek@gmail.com <cherif.abedrazek@gmail.com>
2021-10-08 16:49:42 +0200

    adding preprocesser to dataloaders && adding logs file

--- faultsDetectionDL/training/pt/pt_segmentation_models.py
+++ faultsDetectionDL/training/pt/pt_segmentation_models.py
@@ -7,6 +7,7 @@ Created on Thu Sep  2 19:05:26 2021
 """
 
 import os
+from matplotlib import pyplot as plt
 import numpy as np
 import torch
 import segmentation_models_pytorch as smp
@@ -15,30 +16,42 @@ from faultsDetectionDL.training.pt import TRAIN_PATH,VALID_PATH, IMG_CHANNELS, B
 from torch.utils.data import TensorDataset, DataLoader
 from faultsDetectionDL.utils.image_transformation import images_transformations_list, Trans_Identity
 from faultsDetectionDL.data.pt.faults_dataset import FaultsDataset 
+import json
+from json.decoder import JSONDecodeError
+
+################# Model1
+model_name = "MAnet_resnext101_32x8d"
+ENCODER = 'resnext101_32x8d'
+ENCODER_WEIGHTS = 'imagenet'
+model = smp.MAnet(
+    encoder_name=ENCODER,
+    encoder_weights=ENCODER_WEIGHTS, 
+    in_channels=IMG_CHANNELS,
+    classes=n_classes,
+    activation=activation,
+    decoder_use_batchnorm=True,
+    decoder_channels=[512,512,256,128,64],
+    #decoder_attention_type="scse"
+)
+
+preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
+
 
 ############### Load data
-train_dataset = FaultsDataset(TRAIN_PATH, images_transformations_list)
-valid_dataset = FaultsDataset(VALID_PATH, [Trans_Identity()])
+train_dataset = FaultsDataset(TRAIN_PATH, images_transformations_list, preprocessing=preprocessing_fn, img_bands=IMG_CHANNELS)
+valid_dataset = FaultsDataset(VALID_PATH, images_transformations_list, preprocessing=preprocessing_fn, img_bands=IMG_CHANNELS)
 
 train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=12)
 valid_dataloader = DataLoader(valid_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4)
 
 
-################# Model1
-model = smp.UnetPlusPlus(
-    encoder_name="resnet34",
-    encoder_weights="imagenet", 
-    in_channels=IMG_CHANNELS,
-    classes=n_classes,
-    activation=activation
-)
-
 ################# Optimizer definition
 metrics = [
     smp.utils.metrics.IoU(threshold=0.5),
+    smp.utils.metrics.Fscore()
 ]
 
-LR = 0.0001
+LR = 0.000291
 optimizer = torch.optim.Adam([ 
     dict(params=model.parameters(), lr=LR),
 ])
@@ -46,7 +59,9 @@ optimizer = torch.optim.Adam([
 
 ################# loss definition
 from segmentation_models_pytorch.utils.losses import BCELoss , JaccardLoss
-total_loss = BCELoss()+ JaccardLoss()
+from faultsDetectionDL.training.pt.custom_losses import neighborLoss
+total_loss = BCELoss(torch.tensor(class_weights)) + 0.2*JaccardLoss() 
+loss_name = "weighted_bce_n_jacc"
 
 
 ####################
@@ -68,24 +83,44 @@ valid_epoch = smp.utils.train.ValidEpoch(
     verbose=True,
 )
 
-c_model_name = model_name_template.format(model_name="upp", loss_name="jacc_n_bce")
+
+c_model_name = model_name_template.format(model_name=model_name, loss_name=loss_name)
 c_models_path = os.path.join(models_path, c_model_name)
 if (not os.path.isdir(c_models_path)):
     os.makedirs(c_models_path)
+
+logs_file_path=os.path.join(c_models_path, "logs.json")
+open(logs_file_path, "a").close()
 if __name__ =="__main__":
-        
-    for i in range(0, 40):
+    
+    all_logs={}
+    for i in range(1, 10):
         
         print('\nEpoch: {}'.format(i))
         train_logs = train_epoch.run(train_dataloader)
         valid_logs = valid_epoch.run(valid_dataloader)
-        
+                
         # do something (save model, change lr, etc.)
         torch.save(model, os.path.join(c_models_path,'{}_model.pth'.format(i)))
         torch.cuda.empty_cache()
         print('Model saved!')
-            
-        if i == 25:
-            optimizer.param_groups[0]['lr'] = 1e-5
-            print('Decrease decoder learning rate to 1e-5!')
-
+        
+        ## load json file
+        old_logs=dict()
+        try:
+            with open(logs_file_path, "r") as logs_file:
+                old_logs = json.load(logs_file)
+        except JSONDecodeError:
+            pass
+        
+        old_logs[i]={"train_logs":train_logs, "valid_logs":valid_logs}
+        
+        with open(logs_file_path, "w") as logs_file:
+            json.dump(old_logs, logs_file)
+        
+        
+        if i % 4==0:
+            optimizer.param_groups[0]['lr'] /= 2
+            print('Decrease decoder learning rate by factor 2!')
+    
+    
