9ce2eba9b6b01a84f206cd744b48786c20695a94
unknown <macherif@luxcarta.com>
2021-12-30 16:29:09 +0100

    updating class weights init method

--- faultsDetectionDL/training/pt/__init__.py
+++ faultsDetectionDL/training/pt/__init__.py
@@ -9,13 +9,14 @@ SIZE_X = 256
 SIZE_Y = 256
 IMG_CHANNELS=3
 n_classes=3 #Number of classes for segmentation
-class_weights= [0.6, 0.3, 0.1]
-BATCH_SIZE=4
-activation='softmax'
+class_weights= 1/np.array([0.86556702, 0.09528941, 0.0391435])
+class_weights/=sum(class_weights)
+BATCH_SIZE=8
+activation=None#'softmax2d'
 models_path="./models/buildings/trial1/"
 model_name_template = "{model_name}_{loss_name}"
 
 
-DATA_PATH_TMPL= os.path.abspath("./data/processed/b_n_c_256/{}")
+DATA_PATH_TMPL= os.path.abspath("../DATA_SANDBOX/processed/b_n_c_256/{}")
 TRAIN_PATH = DATA_PATH_TMPL.format("train")
 VALID_PATH = DATA_PATH_TMPL.format("valid")
