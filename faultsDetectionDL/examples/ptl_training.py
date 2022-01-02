5975809217c868639d99776ea813383bc843aa65
cherif.abedrazek@gmail.com <cherif.abedrazek@gmail.com>
2022-01-02 20:39:58 +0100

    updating ptl example

--- faultsDetectionDL/examples/ptl_training.py
+++ faultsDetectionDL/examples/ptl_training.py
@@ -6,11 +6,16 @@ Created on Fri Dec 31 14:42:10 2021
 @author: cherif
 """
 
-from faultsDetectionDL.training.ptl.lightning_segmentation_model import lightningSegModel, FaultsDataModule
+from faultsDetectionDL.training.ptl.lightning_segmentation_model import lightningSegModel
+from faultsDetectionDL.data.ptl.faults_data_module import FaultsDataModule
 from matplotlib import pyplot as plt
 import numpy as np
 import torch
 import pytorch_lightning as pl
+from ray_lightning import RayPlugin
+
+import ray
+ray.init("auto")
 
 train_params = {
     "learning_rate":0.00398107,
@@ -24,8 +29,8 @@ train_params = {
     #"auto_scale_batch_size":"binsearch",
     #"auto_lr_find":True,
     "num_workers":72,
-    "min_epochs":50,
-    "max_epochs":100
+    "min_epochs":40,
+    #"max_epochs":1
     }
 
 TRAIN_DATASET_PATH="./data/processed/b_n_c_256/train"
@@ -42,13 +47,17 @@ data_module.setup(dataset_path="./data/processed/b_n_c_256", encoder_name=c_ligh
                   encoder_weights=c_light_model.encoder_weights,in_channels=in_channels,
                   classes=classes, batch_size=train_params["batch_size"])
 
-c_light_model.fit(data_module)
+
+trainer = pl.Trainer( plugins=[RayPlugin(num_workers=2, use_gpu=True)] ,**c_light_model._get_trainer_params())
+trainer.fit(c_light_model, datamodule=data_module)
 
 
+"""c_light_model.fit(data_module)
 
-
-sample_val = next(iter(data_module.train_dataloader()))
+dataloader_iterator = iter(data_module.val_dataloader())
+sample_val = next(dataloader_iterator)
 preds = c_light_model.model.predict(sample_val[0])
 preds = torch.argmax(preds,1)
 plt.imshow(preds[0])
 
+"""
