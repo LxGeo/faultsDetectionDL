e3a741ede7ca60ac198b284460713abbf3d2cf96
cherif.abedrazek@gmail.com <cherif.abedrazek@gmail.com>
2022-12-09 18:55:04 +0100

    setup file for develop mode

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+if __name__ == "__main__":
+    setup(
+    name='faultsDetectionDL',
+    version='0.0.1',
+    author='sherif-med',
+    description='A package of common modules for deep learning applied on faults',
+    long_description='A set modules utilities for processing and DL',
+    url='https://github.com/LxGeo/faultsDetectionDL.git',
+    keywords='DL, datasets, formulas',
+    python_requires='>=3.7',
+    install_requires=[],
+    py_modules=[]
+    )
